ea00451760760ca1859e0ac101d91ac48f173617
Ayush4Gupta <Ayush4Gupta@users.noreply.github.com>
2026-09-07 00:05:46 +0530

    Exclude local RealImageCompressionTest on CI runner

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,3 +11,9 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
 }
+
+tasks.test {
+    if (!file("C:/Users/ritik/Downloads/pp.ritik.jpg").exists()) {
+        exclude("**/RealImageCompressionTest*")
+    }
+}
