970b31d51d202d426d79584c89335cc94d499f0b
Ayush4Gupta <Ayush4Gupta@users.noreply.github.com>
2026-09-07 00:35:39 +0530

    Configure release keystore generation in CI, disable lint fatal abortion, and upload failure reports

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    lint {
+        abortOnError = false
+        checkReleaseBuilds = false
+    }
 }
 
 dependencies {
